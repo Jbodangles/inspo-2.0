fc4012305a56ffb6aa17915ff9c539b717568d7b
Jbodangles <87615905+Jbodangles@users.noreply.github.com>
2022-04-10 20:17:44 -0400

    discord package changed

--- main.py
+++ main.py
@@ -27,5 +27,31 @@ async def on_message(message):
     if message.content.startswith('/inspire'):
         quote = get_quote()
         await message.channel.send(quote)
+# Initialization
+client = discord.Client()
+
+jokes = [
+  "Mr. Wzrd has the smallest want in all of Omnia",
+  "When hugging Mr. Wzrd you tend to hug his head more than his body",
+  "When kissing Mr. Wzrd either you have to tiptoe, or the other person has to kneel",
+  "Mr. Wzrds legs don't touch the floor when sitting on a stool",
+  "Coming down the stairs must feel like skydiving for Mr. Wzrd",
+  "Mr. Wzrd is the literal definition of down to earth",
+  "Why can't Mr. Wzrd get depressed? He's always looking up!",
+  "You know you are smakk when you can do pull-ups on a door handle, just sk Mr. Wzard"
+]
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('/kitch'):
+        get_joke = random.choice(jokes)
+        await message.channel.send(get_joke)
 
 client.run(token)
